5d63388b70ea2880559c92e5fd14016df01f5fd3
Afonso Benedito <afonso.benedito23@gmail.com>
2020-12-04 14:18:25 +0000

    Descarregar fotos

--- atualizador.py
+++ atualizador.py
@@ -1,4 +1,5 @@
 import requests
+import shutil
 import json
 
 # API KEY:
@@ -7,6 +8,9 @@ key = 'https://api.management.realizasom.com/projects/app/pt/KCLPFYWJcExvInAepy8
 # HTTP request from API KEY
 getter = requests.get(key)
 
+####################################################################################
+########################## Get JSON File ###########################################
+####################################################################################
 
 if (getter.status_code == 200): #If there's no error the json file will be written
     data = getter.json()
@@ -14,3 +18,29 @@ if (getter.status_code == 200): #If there's no error the json file will be writt
         json.dump(data, f, indent=4, ensure_ascii=False) # ensure_ascii=False -> ensures that accented letters do not become ascii
 else: #if there's an error, it will be printed
     print('Problemas a atualizar o quiosque. Tente novamente ou contacte a empresa.\nErro:', getter.status_code)
+
+####################################################################################
+########################## Get file to folder #####################################
+####################################################################################
+
+
+print(data["rooms"][0])
+
+
+
+
+
+# file_url = '13bc7f74-94b4-41da-b193-4d44e9d2c794.mp4'
+# full_url = 'https://api.management.realizasom.com/files/' + file_url
+
+# response = requests.get(full_url, stream = True)
+
+# if r.status_code == 200:
+#     r.raw.decode_content = True # decode_content as to be True, otherwise the file's size will be zero.
+    
+#     with open('./video/imagem.mp4','wb') as f:
+#         shutil.copyfileobj(r.raw, f)
+        
+#     print('Image sucessfully Downloaded: ','filename')
+# else:
+#     print('Image Couldn\'t be retreived')
